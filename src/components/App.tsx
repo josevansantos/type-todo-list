91677a5d998b4e90bf5ec136a825e82f625267aa
Josevan Santos <josevansnt@gmail.com>
2021-10-18 15:02:15 -0300

    feat: add className

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './Navbar';
+import TodoList from './TodoList';
 
 const App = () => {
   return (
-    <div>
+    <div className="uk-container">
       <Navbar></Navbar>
+      <TodoList></TodoList>
     </div>
   );
 };
