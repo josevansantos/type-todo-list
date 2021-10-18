1515820134038830f875667f4979be8c0093a623
josevansantos <josevansantos@users.noreply.github.com>
2021-10-18 21:54:13 +0000

    deploy: 9ed16b0fb8209f3d406c2000526e1e5972b5c71a

---
+++ precache-manifest.1d510313e59b1a0d158af77e3e10df47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0994bb7d6915553cb15933ec6c1e944f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8521c248ada64c72ed75",
+    "url": "/static/js/2.95c2ab61.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.95c2ab61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fcf65e6c2ccb9820bdd",
+    "url": "/static/js/main.1e16a6d3.chunk.js"
+  },
+  {
+    "revision": "4a42fdcbdfd3cf55fea3",
+    "url": "/static/js/runtime-main.1c47a842.js"
+  }
+]);
