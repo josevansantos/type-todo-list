932f011fe29fe43b3f924cb1aaec093e99e58f80
Josevan Santos <josevansnt@gmail.com>
2021-10-19 11:52:49 -0300

    style: new colors and fonts

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,14 +7,22 @@ const Navbar = () => {
       <nav className="uk-navbar">
         <div className="uk-navbar-left">
           <Link to="/type-todo-list/" className="uk-navbar-item uk-logo">
-            My Todos
+            <span uk-icon="icon: list; ratio: 1.5"></span>
           </Link>
+        </div>
+        <div className="uk-navbar-center">
+          <h3 className="uk-text-uppercase uk-text-muted uk-text-middle">
+            My To-Do
+          </h3>
         </div>
         <div className="uk-navbar-right">
           <ul className="uk-navbar-nav">
             <li>
               <Link to="/create">
-                <span uk-icon="icon: plus; ratio: 1.2"></span>
+                <span
+                  uk-icon="icon: plus-circle; ratio: 1.8"
+                  className="uk-text-success"
+                ></span>
               </Link>
             </li>
           </ul>
